14ce66761fb45eba04f475d672838683bc7eac17
Togzhan2003 <120377557+Togzhan2003@users.noreply.github.com>
2023-02-22 19:00:13 +0600

    Add files via upload

---
+++ Lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.open('https://wa.me/87716704279', '_blank');
+  }
+
+  link() {
+    window.open('products.link', '_blank');
+  }
+
+  image(){
+    window.open('products.image', '_blank');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
